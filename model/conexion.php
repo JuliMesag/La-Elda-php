cdd237e08b05855aec15e04d9f1837b874010847
Julian David Mesa <julian98mesa@gmail.com>
2022-06-16 20:30:09 -0500

    cambio en el model

--- model/conexion.php
+++ model/conexion.php
@@ -4,9 +4,9 @@ function conectarBD(){
     $db = mysqli_connect('localhost', 'root', 'password', 'DB_La_Elda');
 
     if($db){
-        echo "se conecto re full mi papá";
+        echo "Conectado";
     }else{
-        echo "no se conecto nada";
+        echo "no se conecto";
     }
 
 }
